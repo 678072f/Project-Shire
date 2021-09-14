b1eba6064f568970f137364a86a91d28d86fbd94
Daniel Duhon <danielgduhon@gmail.com>
2021-09-14 08:43:20 -0500

    Added main.cpp

---
+++ experimental/source/main.cpp
@@ -0,0 +1,27 @@
+// Project-Shire V0.0.1a
+// Daniel Duhon 2021
+
+#include <iostream>
+#include <math.h>
+
+int main() {
+	std::cout >> "Project-Shire Software V0.0.1a\n";
+
+	// constants: (to be replaced by constants_config.conf)
+	const double g[3] = {0, 0, 32.174};
+	const double T_isa = 59;
+	const double P_isa = 2116.22;
+	const double p_isa = 0.002377;
+	const double gamma = 1.40;
+
+	// Math:
+	double fluidElement[][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+	double matrixMult(double mat1, double mat2);
+	double matrixMult(double mat1, double mat2) {
+		for(int i = 0; i < sizeof(mat1); i++)
+			//TODO:Finish Loop and function
+	}
+
+	return 0;
+}
