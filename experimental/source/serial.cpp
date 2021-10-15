0890020bde789caf601f6b1b7db483bb99a13fd0
Daniel Duhon <danielgduhon@gmail.com>
2021-10-14 22:06:22 -0500

    Updated Serial Class

--- experimental/source/serial.cpp
+++ experimental/source/serial.cpp
@@ -1,4 +1,31 @@
 // Serial Communications Link V1.0
-// Daniel Duhon, 9/23/2021
+// Daniel Duhon, 10/14/2021
+// Serial Class
 
 #include <stdlib.h>
+#include "serial.h"
+
+using namespace Shire;
+
+Serial::Serial(void) {
+    // Constructor: Setup serial connection here.
+    std::cout << "Object is being created.\n";
+}
+
+double Serial::readSerial(void) {
+    // Read data from serial port and store to variable.
+    std::cout << "Do stuff...\n";
+    return data;
+}
+
+void Serial::writeSerial(double data, std::string dataString) {
+    // Write data to serial.
+    std::cout << "Do stuff...\n";
+    serialData = data;
+    serialDataString = dataString;
+}
+
+Serial::~Serial() {
+    // Destructor: Uninitialize serial connection here.
+    std::cout << "Object is being deleted.\n";
+}
--- experimental/source/serial.h
+++ experimental/source/serial.h
@@ -1,10 +1,24 @@
+// Serial UART Class V1.0
+// Daniel Duhon, 10/14/2021
+// Serial Class
+
 #ifndef SERIAL_H
 #define SERIAL_H
 
-class Serial {
-public:
+#include <iostream>
+#include <string>
 
-private:
+namespace Shire {
+    class Serial {
+    public:
+        Serial(); // Constructor
+        double readSerial(void); // Public Read from serial function
+        void writeSerial(double data, std::string dataString); // Public write to serial function
+        ~Serial(); // Destructor
+    private:
+        double serialData; // Data stored from serial.
+        std::string serialDataString;
+    }
 }
 
 #endif
