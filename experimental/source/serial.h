ae1eaee1fc8c00736a4c7e528480f12ca3ac25ef
Daniel Duhon <danielgduhon@gmail.com>
2021-10-15 15:00:27 -0500

    Updated serial class

--- experimental/source/serial.h
+++ experimental/source/serial.h
@@ -5,8 +5,13 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-#include <iostream>
-#include <string>
+#include <iostream> // For inputs and outputs
+#include <string> // For C++ Strings
+#include <stdio.h> // Standard input and ouput library
+#include <fcntl.h> // File controls (Linux/UNIX)
+#include <errno.h> // Error functions
+#include <termios.h> // POSIX Terminal control definitions
+#include <unistd.h> // write(), read(), close()
 
 namespace Shire {
     class Serial {
@@ -16,6 +21,7 @@ namespace Shire {
         void writeSerial(double data, std::string dataString); // Public write to serial function
         ~Serial(); // Destructor
     private:
+        int serialPort;
         double serialData; // Data stored from serial.
         std::string serialDataString;
     }
