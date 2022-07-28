a23913dfd19bb961032028c99b9758a82d6648e5
Daniel Duhon <danielgduhon@gmail.com>
2022-07-28 12:08:07 -0500

    Added PFD and NAV python files for testing

---
+++ experimental/source/nav.py
@@ -0,0 +1,9 @@
+# Navigation Display Window
+
+import wx
+
+app = wx.App(False)
+frame = wx.Frame(parent=None, title='Navigation Display (NAV)')
+frame.Show()
+app.MainLoop()
+
---
+++ experimental/source/pfd.py
@@ -0,0 +1,8 @@
+# Primary Flight Display Window
+
+import wx
+
+app = wx.App(False)
+frame = wx.Frame(parent=None, title='Primary Flight Display (PFD)')
+frame.Show()
+app.MainLoop()
